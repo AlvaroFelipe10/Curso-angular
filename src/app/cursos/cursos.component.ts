a81e792b5b7fff2e5d5027f36e3ea41affe596fb
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-07-26 11:13:10 -0300

    introdução aos templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
 
+  nomePortal: string;
+
+  cursos: string [] = ['Java', 'Ext Js', 'Angular'];
+
+  constructor(){this.nomePortal = 'http://loiane.training';
+    }
+
+  ngOnInit(): void {
+
+  }
 }
