8590391b6dc1a1a81d81926361b65feaa63f9d5a
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-08-04 10:14:14 -0300

    guarda de rotas

--- curso-angular2/rotas/src/app/app-routing.module.ts
+++ curso-angular2/rotas/src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
 import { ModuleWithProviders, NgModule } from "@angular/core";
+import { AuthGuard } from "./guards/auth.guard";
 
 
 const appRoutes: Routes = [
-    { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)},
-    { path: 'cursos', loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule)},
+    { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
+      canActivate:[AuthGuard]},
+    {  path: 'cursos', loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
+      canActivate:[AuthGuard]},
    // { path: 'curso/:id', component: CursoDetalheComponent },
    // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
     { path: 'login', component: LoginComponent },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent, canActivate:[AuthGuard] }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
--- curso-angular2/rotas/src/app/app.module.ts
+++ curso-angular2/rotas/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 //import { CursosModule } from './cursos/cursos.module';
-import { AlunosComponent } from './alunos/alunos.component';
+//import { AlunosComponent } from './alunos/alunos.component';
 import { AuthService } from './login/auth.service';
+import { AlunosModule } from './alunos/alunos.module';
+import { AuthGuard } from './guards/auth.guard';
 //import { AlunosModule } from './alunos/alunos.module';
 //import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 //import { CursosService } from './cursos/cursos.service';
@@ -27,9 +29,9 @@ import { AuthService } from './login/auth.service';
     AppRoutingModule,
     FormsModule,
    // CursosModule,
-  //  AlunosModule
+   // AlunosModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
